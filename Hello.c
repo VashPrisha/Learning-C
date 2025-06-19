e4e26cc9b63bff95d78ce45cba158f2377db76a3
VashPrisha <138369405+VashPrisha@users.noreply.github.com>
2025-06-19 21:55:35 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void) // void explicitly tells readers that main takes no arguments, however, the bracket can be left empty too
+{
+    printf("Hello\n");// \n is used to move the cursor to the next line
+    printf("World");
+}
