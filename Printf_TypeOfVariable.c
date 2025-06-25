ed5b864e2d2af23df771c84bb9fd388d2fccb412
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-06-25 13:19:18 +0530

    Delete Printf_TypeOfVariable.c

--- Printf_TypeOfVariable.c
+++
@@ -1,9 +0,0 @@
-//This code basically demonstrates the use of printf function with regards to the declaration of the variable type and format specifier being used.
-
-#include<stdio.h>
-int main()
-{
-    int y = -9;
-    printf("%d\n",y);
-    printf("%u",y);
-}
