fa14731ffb36979073da71f839959c633458305a
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-06-25 13:16:58 +0530

    Add files via upload

---
+++ Printf_TypeOfVariable.c
@@ -0,0 +1,9 @@
+//This code basically demonstrates the use of printf function with regards to the declaration of the variable type and format specifier being used.
+
+#include<stdio.h>
+int main()
+{
+    int y = -9;
+    printf("%d\n",y);
+    printf("%u",y);
+}
