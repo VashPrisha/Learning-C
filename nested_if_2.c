b652fe8389bd01ebaa59c38c8a1421083d26e27a
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-07-07 23:12:31 +0530

    Add files via upload

---
+++ nested_if_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num = 59;
+    if(num>90)
+    {
+        printf("A\n");
+    }
+    if(num<60)
+    {
+        printf("C");
+    }
+}
+//output is generated according to the conditions being true or false, in this case the second statement only stands true.
