95b18adf307a04f67e3b6c0dae94e36c3139c060
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-07-07 23:09:05 +0530

    Add files via upload

---
+++ nested_ifElse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num=92;
+    if(num>90)
+    {
+        printf("you earned an A\n");
+    }else
+    if(num>80)
+    {
+        printf("you earned a B");
+    }
+    return 0;
+}
