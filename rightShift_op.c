2a78ec2056ea2df8a93fbebb187f82e8e977d25b
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-09-07 22:49:49 +0530

    Add files via upload

---
+++ rightShift_op.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;      // binary: 00001010
+    int y = x >> 1;  // shift all bits one place to the right
+
+    printf("Result of %d >> 1 = %d\n", x, y);
+
+    return 0;
+}
