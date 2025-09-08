c507ff2757aca3633f6d300fac4c7acb0cc2853b
Prisha Vashist <138369405+VashPrisha@users.noreply.github.com>
2025-09-08 23:38:19 +0530

    Add files via upload

---
+++ ternary_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+
+    // Syntax: condition ? value_if_true : value_if_false
+    a = 5 < 2 ? 4 : 3;
+
+    printf("Result: %d\n", a);
+
+    return 0;
+}
